622df9ab3daeda1933dcf2b79d83af6973030eab
Jaison Titus <jaison@hasura.io>
2017-12-20 14:57:14 +0530

    Updates

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-var morgan = require('morgan');
 var bodyParser = require('body-parser');
 require('request-debug')(request);
 
 var MDB_API_TOKEN = process.env.MDB_API_TOKEN;
 
 var server = require('http').Server(app);
-
-router.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -18,9 +15,38 @@ app.use(bodyParser.urlencoded({
 
 app.get('/celeb_dob', function(req, res) {
   var name = req.query.name;
-  res.json({
-    birthday: '22-09-1998'
-  });
+  //First search for the celeb  
+  const searchUrl = 'https://api.themoviedb.org/3/search/person?api_key=' + MDB_API_TOKEN + '&language=en-US&query=' + name + '&page=1&include_adult=false';
+  request(searchUrl, function(error, response, body) {    
+    if (response.statusCode === 200) {
+      const jsonResult = JSON.parse(body);
+      if (jsonResult.results.length === 0) {
+        res.json({
+          birthday: 'unknown'
+        });
+        return;
+      }
+      const celebDetailsUrl = 'https://api.themoviedb.org/3/person/' + jsonResult.results[0].id + '?api_key=' + MDB_API_TOKEN + '&language=en-US';
+      request(celebDetailsUrl, function(error, response, body) {         
+        if (response.statusCode === 200) {
+          const jsonResult = JSON.parse(body);          
+          res.json({
+            birthday: jsonResult.birthday
+          });     
+          return;       
+        } else {
+          res.status(500).json({
+                'message': 'TMDB Celeb Details API failed'
+              });
+          return;          
+        }         
+      });
+    } else {
+      res.status(500).json({
+            'message': 'TMDB Search API Failed'
+          });
+    }    
+  }); 
 });
 
 app.listen(8888, function () {
